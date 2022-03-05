bc1ffb04ff3236d69c7ef5d4458feb65e306b600
andreu <andreu@debian.pm>
2022-03-05 11:49:53 +0100

    First commit

---
+++ create_simlinks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ln -sf ~/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
